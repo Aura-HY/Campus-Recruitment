15891b856feaa82a4aedc85e73f3e9bdb57cebbb
csilenti <147641029+csilenti@users.noreply.github.com>
2024-03-27 00:00:58 +0800

    合并适配，并设置部分UI组件

--- src/components/navigationRoutes.vue
+++ src/components/navigationRoutes.vue
@@ -1,13 +1,19 @@
+
 <template>
-    <h1>首页</h1>
-    <h1>消息</h1>
-    <h1>我的</h1>
+    <!--封装底部导航栏 首页+信息+我的-->
+    <var-bottom-navigation v-model:active="active" fixed="true">
+        <var-bottom-navigation-item name="home" label="首页" icon="home" />
+        <var-bottom-navigation-item name="message" label="信息" icon="message-text-outline" />
+        <var-bottom-navigation-item name="user" label="我的" icon="account-circle" />
+    </var-bottom-navigation>
 </template>
 
-<script>
+<script setup>
+    import { ref } from 'vue'
 
+    const active = ref('home')
 </script>
-
+    
 <style scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './assets/iconfont/iconfont.css'
-createApp(App).mount('#app')
+
+import Varlet from '@varlet/ui'
+import '@varlet/ui/es/style'
+
+createApp(App).use(Varlet).mount('#app')
+
+
